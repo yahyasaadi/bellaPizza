34cbb1fa2640ed1c1c0b694840d09e23d976ba05
yahyasaadi <yahyasaadi9219@gmail.com>
2021-11-26 15:22:33 +0300

    Added varibles to get user input

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,17 @@ function Pizza(size, crust, toppings){
         return this.sizePrice + this.crustPrice + this.toppingsPrice;
   }
 }
+
+// Try an array for the prices
+// var conceptName = $('#aioConceptName').find(":selected").text();
+// for radioButton $("input[name='optradio']:checked").val();
+
+$(document).ready(function(){
+    $("#new_order").submit(function(e){
+        e.preventDefault();
+        var pizzaSize = $("#pizzaSizeOptions").find(":selected").text();
+        var crustType = $("#crustOptions").find(":selected").text();
+        var toppingsOption = $("input[name='toppings']:checked").val();
+        alert(toppingsOption);
+    });
+});
